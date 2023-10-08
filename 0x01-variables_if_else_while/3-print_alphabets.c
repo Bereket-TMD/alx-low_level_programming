863ad9ad90b4f5dcf06c5cb7d487fad7fd0af93a
Bereket <beaky2193@gmail.com>
2023-10-08 13:54:06 +0300

    3-prints_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - function prints alphabet bot in upper and lower case.
+ *
+ * Return: 0
+*/
+int main(void)
+	{
+		char b;
+	for (b = 'a'; b <= 'z'; b++)
+	putchar(b);
+	for (b = 'A'; b <= 'z'; b++)
+	putchar(b);
+	putchar('\n');
+	return (0);
+	}
