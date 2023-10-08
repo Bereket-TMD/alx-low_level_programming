5b6ded75d947e1f24f55c624028bac14fb5e0735
Bereket <beaky2193@gmail.com>
2023-10-08 13:16:59 +0300

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+	 * main - the function putchar from a to z.
+	 * Return: 0 (success)
+ */
+
+int main(void)
+
+	{
+	char b;
+		for (b = 'a'; b <= 'z'; b++)
+		putchar(b);
+		putchar('\n');
+
+	return (0);
+
+	}
